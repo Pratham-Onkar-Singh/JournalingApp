09318c9433c63b982c495462ab73a735ed351c3b
Pratham Onkar Singh <prathamaj09@gmail.com>
2025-09-15 13:15:53 +0530

    saving myself

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config()
 
 export const connect_mongodb = async () => {
     try {
-        await mongoose.connect("mongodb+srv://test_db:0FaeYYOKeRauc0Mh@cluster0.unekmnd.mongodb.net/mini-project");
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("Connected to DB!");
     } catch (error) {
         console.error();
